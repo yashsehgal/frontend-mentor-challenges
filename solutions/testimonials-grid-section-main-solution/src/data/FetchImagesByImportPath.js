fa4fa513d5e21692393dffa736b2f0339fe53db2
yashsehgal <yashsehgal.work@gmail.com>
2021-12-30 20:25:13 +0530

    feat: old solutions, new challenges added

---
+++ solutions/testimonials-grid-section-main-solution/src/data/FetchImagesByImportPath.js
@@ -0,0 +1,17 @@
+import BGPatternQuotation from '../assets/bg-pattern-quotation.svg';
+import ImageDaniel from '../assets/image-daniel.jpg';
+import ImageJeanette from '../assets/image-jeanette.jpg';
+import ImageJonathan from '../assets/image-jonathan.jpg';
+import ImageKira from '../assets/image-kira.jpg';
+import ImagePatrick from '../assets/image-patrick.jpg';
+
+const ImageExports = {
+  "BGPatternQuotation": BGPatternQuotation,
+  "ImageDaniel": ImageDaniel,
+  "ImageJeanette": ImageJeanette,
+  "ImageJonathan": ImageJonathan,
+  "ImageKira": ImageKira,
+  "ImagePatrick": ImagePatrick
+}
+
+export default ImageExports; 
